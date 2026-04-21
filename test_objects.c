53c02418ef507c4c94da73743e841cdead202423
tharith0306 <tharithreddy@gmail.com>
2026-04-21 21:14:35 +0530

    Update test_objects.c

--- test_objects.c
+++ test_objects.c
@@ -1,6 +1,6 @@
 // test_objects.c — Phase 1 test program
 //
-// Compile and run:
+// Compile and run: M
 //   gcc -Wall -Wextra -O2 -o test_objects test_objects.c object.c -lcrypto 
 //   ./test_objects
 
